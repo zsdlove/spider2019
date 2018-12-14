76f0260031756ec3abddf47de5d5f3a64b5cd896
zsdlove <2096350083@qq.com>
2018-12-14 13:57:43 +0800

    add spider deep

--- spiderMain.py
+++ spiderMain.py
@@ -7,18 +7,16 @@ from bs4 import BeautifulSoup
 threads = []
 start = time.time()
 import urllib
-
+deep=2 #控制爬行深度
 global domain
 # 建立队列，存储爬取网址
 que = Queue(100000)
 # 线程类，每个子线程调用，继承Thread类并重写run函数，以便执行我们的爬虫函数
-global cout
-cout = 0
 proxy = ""
 lock = threading.Lock()
 file = open('srcaptest.txt', 'a+')
 oldurl=[]
-
+threadsNum = 2
 class myThread(threading.Thread):
     def __init__(self, que):
         threading.Thread.__init__(self)
@@ -37,11 +35,11 @@ class myThread(threading.Thread):
 #返回url
 def download(url):
     res=requests.get(url,timeout=3)
-    return res
+    return res.content
 
 def doqueput(urls,que):
     for  newurl in urls:
-        if IsSpidered(newurl):
+        if IsSpidered(newurl) and IsOverDeep(newurl):
             print("checked，新url，没错")
             que.put(newurl)
             oldurl.append(newurl)
@@ -50,7 +48,6 @@ def doqueput(urls,que):
             print("队列大小:"+str(que.qsize()))
 # 解析url，放入队列
 def parseURL(que, urls):
-        #res = download(newurl)
         doqueput(urls,que)
 
 def getdomain(url):
@@ -62,27 +59,20 @@ def getdomain(url):
 
 def crawler(que):
     with lock:
-        e=threading.Event()
-        if que.qsize==0:
-            e.wait()
         baseurl = que.get(timeout=3)
-        print(baseurl)
-        #try:
-        response = requests.get(baseurl, timeout=4)
-        #except:
-        content = response.content
+        content = download(baseurl)
         soup = BeautifulSoup(content, 'html.parser')
         AllHrefTag = soup.find_all('a')
         newurls = []
         for hreftag in AllHrefTag:
-             #try:
-               url = str(hreftag['href'])
-               if "http://" in url and isinnerurl(url):
-                   newurls.append(url)
-                   global cout
-                   cout=cout+1
-             #except:
-              # continue
+             try:
+                   url = str(hreftag['href'])
+                   if "http:" in url and isinnerurl(url):
+                       newurls.append(url)
+                   elif "https:" in url and isinnerurl(url):
+                       newurls.append(url)
+             except:
+               continue
         newurls=set(newurls)
         parseURL(que, newurls)
 
@@ -92,7 +82,6 @@ def IsSpidered(url):
     else:
         return True
     return True
-	
 def isinnerurl(url):
     urldata=urllib.parse.urlsplit(url)
     if domain==urldata[1]:
@@ -100,16 +89,46 @@ def isinnerurl(url):
     else:
         return False
     return True
-	
+
 def urlclean(url):
     pass
 
-if __name__ == '__main__':
+
+
+def IsOverDeep(url):
+    urldata=urllib.parse.urlsplit(url)
+    SplitResult=str(urldata[2]).split('/')
+    spiderdeep=len(SplitResult)-1
+    print(len(SplitResult)-1)
+    print("目前爬行深度："+str(spiderdeep))
+    if  spiderdeep>=deep:
+        return False
+    else:
+        return True
+    return True
+
+# 获取代理
+def getproxy():
+    flag = True
+    ipdic = {'http': 'http://119.114.125.253:58424'}
+    while (flag):
+        try:
+            res = requests.get('http://api.ip.data5u.com/dynamic/get.html?order=4a2c1091b15515eff96cadddf0228a16&sep=6',timeout=5)
+            # res=requests.get('http://dps.kdlapi.com/api/getdps/?orderid=994460608536855&num=1&pt=1&ut=1&sep=4',timeout=3)
+            ip = res.text.replace(";", "").replace("\n", "").replace("|", "")
+            ipdic = {'http': 'http://' + ip}
+            print(ipdic)
+            flag = False
+        except:
+            continue
+    return ipdic
+
+def Main():
     global domain
     url = 'http://www.freebuf.com'
     domain=getdomain(url)
     que.put(url)
-    for i in range(1, 10):
+    for i in range(threadsNum):
         thread = myThread(que)
         thread.start()
         threads.append(thread)
@@ -119,3 +138,6 @@ if __name__ == '__main__':
     end = time.time()
     # 输出时间并结束
     print("The total time is:", end - start)
+
+if __name__ == '__main__':
+    Main()
